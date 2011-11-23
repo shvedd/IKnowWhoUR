56702bba75bc86c6dcdb6fb778e9281faad8d0f6
Shved <shved@xakep.ru>
2011-11-23 17:22:05 +0600

    totally rewrited

--- comment.php
+++ comment.php
@@ -1,78 +1,122 @@
 <?php
 ###################################################
 # IKnowWhoUR: Namba Comment Sender v0.1 beta      #
-# © 2011 by Shved								  #
-# shved(at)xakep(dot)ru 	 	   			      #
+# © 2011 by Mekan Shved Bayryew                   #
+# shved(at)xakep(dot)ru                           #
 ###################################################
-
- set_time_limit(0); 
  
- $date   = date("F j, Y");
- $time   = date("g:i a");
- $id 	 = '#####';					//ID поста 
- $icqnum = '######';				//ICQ номер, на который хотим получать уведомления
- $ip     = $_SERVER['REMOTE_ADDR'];    
- 
- if(isset($_SERVER['HTTP_REFERER']))
- {
- 	$referer = trim($_SERVER['HTTP_REFERER']);
- 	if($referer == "http://blogs.namba.kg/post.php?id=$id")
- 	{
-  		$fgc = file_get_contents("http://blogs.namba.kg/post.php?id=$id");
-		if($fgc)
-		{
-			$fp = fopen('blog.txt', 'w');
-			$fw = fwrite($fp,$fgc);
-				  fclose($fp);
-		}		
- 	}else exit();
- }
-
- if(file_exists('blog.txt')){
- 	$file = ('result.txt');
- 	$fp2  = fopen('blog.txt','r');
- 	while(!feof($fp2)){
- 		$line = fgets($fp2);
-	 	if(preg_match('|<p>(.*)</p>|Uis', $line, $result)) //Составить регулярку нормальную
-	 	{
-	 		foreach($result as $match)
-	 		{
-	 			file_put_contents($file, $match, FILE_APPEND);
-	 		}
-	 	}
- 	}
- 	fclose($fp2);
- }
- 
- if(file_exists('result.txt'))
- {
-	include('WebIcqLite.class.php');
-	
-	$comment = "Господин, в вашем блоге #$blog_name# новый коммент.\r\n
-				IP комментатора: $ip\r\n
-				Никнейм: $name\r\n
-				Коммент: $message";
-
-	$comment = iconv("UTF-8","cp1251",$comment);
-
-	define('UIN', '#########');		 //Бот uin  (регаем на icq.com)
-	define('PASSWORD', '#########'); //Бот пасс
-
-	$icq = new WebIcqLite();
-
-	if($icq->connect(UIN, PASSWORD))
-	{
-	   if(!$icq->send_message("$icqnum", "$comment"))
-	   {
-	   	$icq->error();
-	   }
-	   $icq->disconnect();
-	}
- }
-
  Header("Content-type: image/png"); 
- $im = ImageCreateFromGif('gif.gif');
+ $im = ImageCreateFromGif('img/yeah.gif');
  ImageGif($im);
  ImageDestroy($im);
+  
+ $sendto = "190739303";     //ICQ номер на который слать мессаги
+ 
+ $ip     = $_SERVER['REMOTE_ADDR'];
+ $id     = 32079;
+ $url    = "http://blogs.namba.kg/post.php?id=$id";
+ /*
+ $host   = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+ $url2   = "http://".$host.$_SERVER['REQUEST_URI'];
+ $arg    = parse_url($url2,PHP_URL_FRAGMENT);
+
+ if(!empty($arg['fragment'])){
+    $cid = $arg['fragment']
+ }
+ */
+
+ if(isset($_SERVER['HTTP_REFERER']))
+ {
+    $referer = trim($_SERVER['HTTP_REFERER']);
+    
+    if($referer == "http://blogs.namba.kg/post.php?id=$id")
+    {
+        $fgc = file_get_contents("http://blogs.namba.kg/post.php?id=$id");
+        
+        if(file_exists('blog.txt'))
+        {
+            if(filesize($fgc) != filesize('blog.txt'))
+            {
+                file_put_contents('blog.txt',$fgc);
+            }else exit();
+        } else {
+            file_put_contents('blog.txt',$fgc);
+        }
+        $blog = file_get_contents('blog.txt');
+    }else die();
+ }else die();
+ 
+ //Узнаем title Блога 
+ preg_match("/<title>(.*)<\/title>/siU",$blog, $matches);
+ $btitle = $matches[1];
+ $btitle = preg_replace('/\s\s/','',$btitle);
+
+##################################################################################
+ //Подключаем класс HTML_DOM_PARSER
+ include_once('html_dom.php');
+ //Клева жэ что есть такое счастье? ^_^
+
+ function namba_comments($i)
+ {
+    $source = file_get_html('blog.txt');
+
+    foreach($source->find('div.commentlist') as $comments)
+    {
+       $count = count($comments);
+       $parsed['username'] = trim($comments->find('a.username', $i)->plaintext);
+       $parsed['comment']  = trim($comments->find('p', $i)->plaintext);       
+       
+       $array[] = $parsed;
+    }
+
+    $source->clear();
+    unset($source);
+
+    return $array;
+ }
+###################################################################################
+
+ //Запускаем функцию поиска автаров и их комменты
+ $array = namba_comments(0);
+
+ foreach($array as $v)
+ {   
+    $username = $v["username"];
+    $message  = $v["comment"];
+    //Удаляем лишние html'лские пробелы
+    $message  = preg_replace('/&nbsp;/', '', $message);
+ }
+
+ if(!empty($username))
+ {
+ 	//Подключаем класс для работы с аськой
+    include('WebIcqLite.class.php');
+    //Формируем сообщение для отправки в аську
+    $added_comment = "Господин, в вашем блоге ~$btitle~ ($url) новый коммент.\r\nIP комментатора: $ip\r\nНикнейм: $username\r\nКоммент: $message";
+    
+    $added_comment = iconv("UTF-8","cp1251",$added_comment);
+
+    define('UIN', 626566206);       //Бот uin  (регаем на icq.com)
+    define('PASSWORD', 'wc3dfclm'); //Бот пасс
+
+    $icq = new WebIcqLite();
+    
+    if($icq->connect(UIN, PASSWORD))
+    {
+       if(!$icq->send_message("$sendto", "$added_comment"))
+       {
+        $icq->error();
+       }
+    }
+    $icq->disconnect();
+
+ }else echo "Блять хуй пойми че те еще нужно?!";
+
+ die();
+
+#######################################################################
+# Осталось сделать так, чтобы выбирался только последний комментарий. #
+# Но это хуйня.                                                       #
+#######################################################################
 
 ?>
